758587c027ba50334b2e8b7a57904eec691ad4de
Wojciech Pietrzak <wojciechpietrzak2003@gmail.com>
2023-01-19 20:46:11 +0100

    add functionality in js

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const price = document.querySelector("#price");
+const people = document.querySelector("#people");
+const tip = document.querySelector("#tip");
+
+const btn = document.querySelector(".main__btn");
+const error = document.querySelector(".main__info--error");
+const info = document.querySelector(".main__info--cost");
+const resultSpan = document.querySelector(".main__info--span");
+
+const countBill = () => {
+	const newPrice = parseFloat(price.value);
+	const newPeople = parseInt(people.value);
+	const newTip = parseFloat(tip.value);
+
+	const resultValue = (newPrice + newPrice * newTip) / newPeople;
+	resultSpan.textContent = resultValue.toFixed(2);
+	console.log(resultValue);
+};
+
+btn.addEventListener("click", () => {
+	if (price.value === "" || people.value === "" || tip.value === 0) {
+		info.style.display = "none";
+		error.innerHTML = "Complete all fields!";
+	} else {
+		info.style.display = "block";
+		error.innerHTML = "";
+
+		countBill();
+	}
+});
